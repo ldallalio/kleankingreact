b82738b525dde93ba47581a63b72414080e42ac8
Logan Dallalio <logan@dallalioweb.dev>
2022-08-06 05:52:12 -0500

    creating social header

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import SocialHeader from '../components/SocialHeader';
+
+function Home() {
+	return (
+		<>
+			<SocialHeader />
+			<Header />
+		</>
+	);
+}
+
+export default Home;
