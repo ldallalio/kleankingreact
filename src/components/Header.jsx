1896700cc14783858a3c7facf71797301f4ea312
Logan Dallalio <logan@dallalioweb.dev>
2022-08-07 01:05:32 -0500

    Adding Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
-
+import Logo from '../assets/logo/KleanKingLogo.png';
 function Header() {
-	return <div>Header</div>;
+	return (
+		<div className='headerContainer'>
+			<div className='logo'>
+				<img src={Logo} alt='Klean King Logo' />
+			</div>
+			<div className='navMenu'>
+				<ul>
+					<li className='menuItem active'>
+						<a href='#'>Home</a>
+					</li>
+					<li className='menuItem'>
+						<a href='#'>About</a>
+					</li>
+					<li className='menuItem'>
+						<a href='#'>Our Services</a>
+					</li>
+					<li className='menuItem'>
+						<a href='#'>Our Work</a>
+					</li>
+					<li className='menuItem'>
+						<a href='#'>Contact</a>
+					</li>
+				</ul>
+			</div>
+			<div className='getQuote'>
+				<a href='#'>Get a Quote</a>
+			</div>
+		</div>
+	);
 }
 
 export default Header;
