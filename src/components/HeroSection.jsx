474cfb8dad698e4e22e3418feb8eb75e0f2ecea3
Logan Dallalio <logan@dallalioweb.dev>
2022-08-07 01:35:16 -0500

    Setting Up Hero Section

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HeroSection() {
+	return (
+		<div className='heroContainer'>
+			<div className='heroContent'>
+				<h1>Hero Content</h1>
+			</div>
+		</div>
+	);
+}
+
+export default HeroSection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import SocialHeader from '../components/SocialHeader';
+import Hero from '../components/HeroSection';
 
 function Home() {
 	return (
 		<>
 			<SocialHeader />
 			<Header />
+			<Hero />
 		</>
 	);
 }
