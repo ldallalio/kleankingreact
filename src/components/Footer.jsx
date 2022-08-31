cecd1b236d5be7faeef899b810daa5a4f95d59af
Logan Dallalio <logan@dallalioweb.dev>
2022-08-30 23:14:50 -0500

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,181 @@
 import React from 'react';
 
 function Footer() {
-	return <div>Footer</div>;
+	return (
+		<div className='footerContainer'>
+			<div className='footerTopSection'>
+				{/* Section 1 */}
+				<div className='footerLogoArea'>
+					<div className='footerLogo'></div>
+					<div className='footerLogoText'>
+						At Klean King Air Duct Cleaning and Restoration, we strive to
+						provide customers with the best customer service possible
+					</div>
+					<div className='footerSocialIcons'>
+						<div className='fb'>
+							<a href='https://www.facebook.com/kleankingcarpet/' target='_'>
+								{/* facebook */}
+								<svg
+									width='35'
+									height='35'
+									viewBox='0 0 48 48'
+									fill='none'
+									xmlns='http://www.w3.org/2000/svg'>
+									<circle cx='24' cy='24' r='24' fill='#164687' />
+									<g clip-path='url(#clip0_12_308)'>
+										<path
+											d='M20.8153 36C22.4699 35.9944 24.13 35.9325 25.7846 35.8313V24.1378H29.0827L29.4825 19.9363H25.7902V17.2702C25.7902 16.6796 26.2621 16.2015 26.8451 16.2015H29.488V12H25.3127C22.8197 12 20.8153 14.0361 20.8153 16.5446V19.9363H18.4V24.1378H20.8153V36Z'
+											fill='white'
+										/>
+									</g>
+									<defs>
+										<clipPath id='clip0_12_308'>
+											<rect
+												width='11.0769'
+												height='24'
+												fill='white'
+												transform='translate(18.4 12)'
+											/>
+										</clipPath>
+									</defs>
+								</svg>
+							</a>
+							<a href='#' className='insta'>
+								<svg
+									width='35'
+									height='35'
+									viewBox='0 0 48 48'
+									fill='none'
+									xmlns='http://www.w3.org/2000/svg'>
+									<circle cx='24' cy='24' r='24' fill='#C52026' />
+									<g clip-path='url(#clip0_12_302)'>
+										<path
+											d='M35.92 19.04C35.88 17.76 35.64 16.88 35.36 16.12C35.04 15.32 34.6 14.6 33.96 14C33.36 13.4 32.64 12.92 31.84 12.6C31.08 12.32 30.2 12.12 28.92 12.04C27.68 12 27.24 12 24 12C20.76 12 20.32 12 19.04 12.08C17.76 12.12 16.88 12.36 16.12 12.64C15.32 12.92 14.6 13.4 14 14C13.4 14.6 12.92 15.32 12.6 16.12C12.32 16.88 12.12 17.76 12.04 19.04C12 20.32 12 20.76 12 24C12 27.24 12 27.68 12.08 28.96C12.12 30.24 12.36 31.12 12.64 31.88C12.96 32.68 13.4 33.4 14.04 34C14.64 34.6 15.36 35.08 16.16 35.4C16.92 35.68 17.8 35.88 19.08 35.96C20.32 36 20.76 36 24 36C27.24 36 27.68 36 28.96 35.92C30.24 35.88 31.12 35.64 31.88 35.36C33.48 34.72 34.76 33.48 35.4 31.84C35.68 31.08 35.88 30.2 35.96 28.92C36 27.68 36 27.24 36 24C36 20.76 36 20.32 35.92 19.04ZM33.76 28.84C33.72 30 33.52 30.64 33.36 31.08C32.96 32.12 32.12 32.96 31.08 33.36C30.64 33.52 30.04 33.72 28.84 33.76C27.6 33.84 27.2 33.84 24 33.84C20.8 33.84 20.4 33.84 19.16 33.76C18 33.72 17.36 33.52 16.92 33.36C16.4 33.16 15.92 32.88 15.56 32.48C15.16 32.08 14.84 31.64 14.68 31.12C14.52 30.68 14.32 30.08 14.28 28.88C14.16 27.6 14.16 27.2 14.16 24C14.16 20.8 14.16 20.4 14.24 19.16C14.28 18 14.48 17.36 14.64 16.92C14.84 16.4 15.16 15.92 15.56 15.56C15.96 15.16 16.4 14.84 16.92 14.68C17.36 14.52 17.96 14.32 19.16 14.28C20.4 14.16 20.8 14.16 24 14.16C27.2 14.16 27.6 14.16 28.84 14.24C30 14.28 30.64 14.48 31.08 14.64C31.6 14.84 32.08 15.12 32.44 15.52C32.84 15.92 33.16 16.36 33.32 16.88C33.48 17.32 33.68 17.92 33.72 19.12C33.84 20.4 33.84 20.8 33.84 24C33.84 27.2 33.84 27.6 33.76 28.84Z'
+											fill='white'
+										/>
+										<path
+											d='M24 17.8403C20.6 17.8403 17.84 20.6003 17.84 24.0003C17.84 27.4003 20.6 30.1603 24 30.1603C27.4 30.1603 30.16 27.4003 30.16 24.0003C30.16 20.6003 27.4 17.8403 24 17.8403ZM24 28.0003C21.8 28.0003 20 26.2003 20 24.0003C20 21.8003 21.8 20.0003 24 20.0003C26.2 20.0003 28 21.8003 28 24.0003C28 26.2003 26.2 28.0003 24 28.0003Z'
+											fill='white'
+										/>
+										<path
+											d='M31.84 17.5997C31.84 18.3997 31.2 19.0397 30.4 19.0397C29.6 19.0397 28.96 18.3997 28.96 17.5997C28.96 16.7997 29.6 16.1597 30.4 16.1597C31.2 16.1597 31.84 16.7997 31.84 17.5997Z'
+											fill='white'
+										/>
+									</g>
+									<defs>
+										<clipPath id='clip0_12_302'>
+											<rect
+												width='24'
+												height='24'
+												fill='white'
+												transform='translate(12 12)'
+											/>
+										</clipPath>
+									</defs>
+								</svg>
+							</a>
+						</div>
+					</div>
+				</div>
+
+				{/* Section 2 */}
+				<div className='footerLinks'>
+					{/* Col 1 Quick Links */}
+					<div className='quickLinksCol'>
+						<div className='footerLinksHeader'>Quick Links</div>
+						<a className='footerLink'>Home</a>
+						<a className='footerLink'>About</a>
+						<a className='footerLink'>Our Services</a>
+						<a className='footerLink'>Our Work</a>
+						<a className='footerLink'>Contact</a>
+					</div>
+					{/* Col 2 Contact Information */}
+					<div className='contactInfoCol'>
+						<div className='footerLinksHeader'>Contact Information</div>
+						<div className='footerLink'>
+							<svg
+								width='24'
+								height='24'
+								viewBox='0 0 24 24'
+								fill='none'
+								xmlns='http://www.w3.org/2000/svg'>
+								<g clip-path='url(#clip0_12_289)'>
+									<path
+										d='M22.1053 15.8021C20.6248 15.8021 19.1748 15.571 17.8004 15.1175C17.1297 14.8863 16.368 15.0644 15.9272 15.5141L13.2025 17.5718C10.0762 15.9032 8.07537 13.9036 6.4295 10.8L8.43159 8.1398C8.93559 7.6358 9.11624 6.89809 8.90024 6.20716C8.44295 4.824 8.21055 3.37263 8.21055 1.89474C8.21055 0.850086 7.36047 0 6.31581 0H1.89474C0.850086 0 0 0.850086 0 1.89474C0 14.0829 9.91706 24 22.1053 24C23.1499 24 24 23.1499 24 22.1053V17.6969C24 16.6522 23.1499 15.8021 22.1053 15.8021Z'
+										fill='#C52026'
+									/>
+								</g>
+								<defs>
+									<clipPath id='clip0_12_289'>
+										<rect width='24' height='24' fill='white' />
+									</clipPath>
+								</defs>
+							</svg>
+							<a href='#'>318-323-8722</a>
+						</div>
+						<div className='footerLink'>
+							<svg
+								width='24'
+								height='24'
+								viewBox='0 0 24 24'
+								fill='none'
+								xmlns='http://www.w3.org/2000/svg'>
+								<g clip-path='url(#clip0_12_276)'>
+									<path
+										d='M21.8908 2.85938H2.10951C1.78481 2.85938 1.48083 2.9392 1.20581 3.07078L11.9533 13.8182L14.3621 11.5032C14.3621 11.5032 14.3623 11.503 14.3624 11.5029C14.3624 11.5028 14.3626 11.5027 14.3626 11.5027L22.7948 3.07097C22.5197 2.9393 22.2156 2.85938 21.8908 2.85938Z'
+										fill='#C52026'
+									/>
+									<path
+										d='M23.789 4.06543L15.8538 12.0002L23.7887 19.935C23.9203 19.66 24.0001 19.356 24.0001 19.0313V4.9688C24.0001 4.64429 23.9204 4.34035 23.789 4.06543Z'
+										fill='#C52026'
+									/>
+									<path
+										d='M0.211406 4.06543C0.0798281 4.34045 0 4.64443 0 4.96913V19.0316C0 19.3561 0.0797344 19.6602 0.211219 19.9351L8.14636 12.0004L0.211406 4.06543Z'
+										fill='#C52026'
+									/>
+									<path
+										d='M14.8596 12.9942L12.4504 15.3095C12.3131 15.4469 12.1332 15.5155 11.9533 15.5155C11.7735 15.5155 11.5935 15.4469 11.4562 15.3095L9.14079 12.9941L1.20551 20.9289C1.48057 21.0606 1.78474 21.1405 2.10954 21.1405H21.8908C22.2155 21.1405 22.5195 21.0607 22.7945 20.9291L14.8596 12.9942Z'
+										fill='#C52026'
+									/>
+								</g>
+								<defs>
+									<clipPath id='clip0_12_276'>
+										<rect width='24' height='24' fill='white' />
+									</clipPath>
+								</defs>
+							</svg>
+							<a href='#'>kleankingcarpet@att.net</a>
+						</div>
+
+						<div className='footerLink'>
+							<svg
+								width='24'
+								height='24'
+								viewBox='0 0 24 24'
+								fill='none'
+								xmlns='http://www.w3.org/2000/svg'>
+								<path
+									d='M12 0C7.03711 0 3 4.03758 3 9C3 15.6397 11.3037 23.5312 11.6573 23.8643C11.7539 23.9546 11.877 24 12 24C12.123 24 12.2461 23.9546 12.3428 23.8643C12.6963 23.5312 21 15.6397 21 9C21 4.03758 16.9629 0 12 0ZM12 14C9.24314 14 6.99998 11.7569 6.99998 9C6.99998 6.24314 9.24319 3.99998 12 3.99998C14.7568 3.99998 17 6.24319 17 9C17 11.7568 14.7569 14 12 14Z'
+									fill='#C52026'
+								/>
+							</svg>
+							<a href='#'>2205 Justice St. Suite B Monroe, LA 71201</a>
+						</div>
+					</div>
+					{/* Col 3 Subscribe to our Newsletter */}
+					{/* <div className='subscribeCol'>
+						<div className='footerLinksHeader'>Subscribe to Our Newsletter</div>
+						<div className='footerLink'>Link 1</div>
+						<div className='footerLink'>Link 2</div>
+					</div> */}
+				</div>
+			</div>
+			{/* Section 3 */}
+			<div className='copyright'>
+				@2022 <a href='#'>Klean King</a> all rights reservered.
+			</div>
+		</div>
+	);
 }
 
 export default Footer;
