939c6e7ca077a9bcf978a53dcd924f336e909af0
Logan Dallalio <logan@dallalioweb.dev>
2022-10-12 22:52:40 -0500

    Add Google Analytics

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import ReactGA from "react-ga4";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactGA.initialize(process.env.REACT_APP_GA_ID);
+if (process.env.REACT_APP_GA_ID != undefined) {
+	console.log("GA4 Connected");
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<App />
